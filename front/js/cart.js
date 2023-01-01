30c7287485898ff6f304ae98b8c0f5b50cc9f56f
Shasha <natasha@Shashas-MacBook-Air.local>
2023-01-01 22:53:19 +0100

    add to cart variables, loops, innerhtml and inrerpolqtions

--- front/js/cart.js
+++ front/js/cart.js
@@ -15,6 +15,7 @@ for (let i = 0; i < cartJson.length; i++) { // created loop for cartJson to disp
     let id = cartJson[i]['id'];
     let color = cartJson[i]['color'];
     let quantity = cartJson [i]['quantity'];
+    quantity = JSON.stringify(quantity)
 
    fetch("http://localhost:3000/api/products/"+ id) // fetching product data
   .then((response) => {
@@ -31,13 +32,13 @@ for (let i = 0; i < cartJson.length; i++) { // created loop for cartJson to disp
                 <div class="cart__item__content">
                   <div class="cart__item__content__description">
                     <h2>${product.name}</h2>
-                    <p>${product.colors}</p>
+                    <p>${color}</p>
                     <p>${product.price} €</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>${product.quantity} : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="1">
+                      <p>Qty:</p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="deleteItem">Supprimer</p>
